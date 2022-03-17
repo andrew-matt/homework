ec304180bab568cdeb1a3329c3b976b0c637179a
andrew-matt <andrew.matt@ya.ru>
2022-03-17 18:16:50 +0300

    add navigation menu

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
 import {PATH} from "./Pages";
+import s from './HW5.module.css'
+
+const image = <img src={'https://i0.wp.com/www.pnglib.com/wp-content/uploads/2020/08/grey-menu-icon_5f34586f4553a.png?w=2000&ssl=1'} alt={'nav-menu'} width={25} height={25} />
 
 function Header() {
     return (
-        <div>
-            <NavLink to={PATH.PRE_JUNIOR}>pre-junior</NavLink>---
-            <NavLink to={PATH.JUNIOR}>junior</NavLink>---
-            <NavLink to={PATH.JUNIOR_PLUS}>junior+</NavLink>
+        <div className={s.header}>
+            <NavLink to={PATH.PRE_JUNIOR} className={s.link}>pre-junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.link}>junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={s.link}>junior+</NavLink>
+            <div className={s.block}>{image}</div>
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import s from '../HW5.module.css'
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
+        <div className={s.errorPage}>
+            <div className={s.errNumber}>404</div>
             <div>Page not found!</div>
             <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
         </div>
